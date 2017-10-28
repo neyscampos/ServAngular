47ccbd1491a495b19e01fcf66ac37fc3f6b141a5
Joao Santana <wozefacu@cartelera.org>
2017-10-28 09:42:50 -0200

    ajustes classe pesssoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,89 @@
 
 public class Pessoa {
 
+	private Integer codigo;
+	private String nome;
+	private String cidade;
+	private Integer idade;
+
+	public Pessoa(Integer codigo, String nome, String cidade, Integer idade) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.cidade = cidade;
+		this.idade = idade;
+	}
+
+	public Pessoa() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "[" + codigo + "]" + nome + ", " + cidade + ", " + idade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
 }
